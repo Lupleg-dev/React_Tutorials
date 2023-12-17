726a36763abd61eb65984cbc7f3bc9c318655d29
marksikaundi <emmanuelmark117@gmail.com>
2023-12-17 11:48:16 +0200

    Fix import casing in layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
-import Footer from "@/components/footer";
-import Header from "@/components/header";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
