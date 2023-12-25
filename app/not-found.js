6b2259b82cfdbdfcc6b28504f5e7d621adda07d4
marksikaundi <emmanuelmark117@gmail.com>
2023-12-25 18:26:32 +0200

    Update font sizes in NotFound component

--- app/not-found.js
+++ app/not-found.js
@@ -7,10 +7,10 @@ export default function NotFound() {
     <div className="flex flex-col h-screen justify-center items-center bg-gray-200">
       <div className="text-center">
         <h1 className="text-6xl font-bold">404</h1>
-        <p className="text-2xl mt-4">
+        <p className="text-4xl mt-4">
           Oops! The page you are looking for does not exist.
         </p>
-        <p className="text-lg mt-2">
+        <p className="text-2xl mt-2">
           You may have mistyped the address or the page may have moved.
         </p>
       </div>
