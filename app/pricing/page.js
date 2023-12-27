66fd0a66b437b106eaade6b4e14b23717d062b3b
marksikaundi <emmanuelmark117@gmail.com>
2023-12-27 08:59:43 +0200

    added content in price plan

--- app/pricing/page.js
+++ app/pricing/page.js
@@ -1,27 +1,64 @@
 import { Button } from "@/components/ui/button";
 
-export default function Component() {
+export default function PricePlan() {
   return (
     <section className="min-h-screen w-full py-12 bg-gradient-to-r from-gray-50 to-gray-100 dark:from-zinc-900 dark:to-zinc-800 flex items-center justify-center">
+      
       <div className="container px-4 md:px-6">
+      <div>
+              <h3 className="text-3xl p-8 font-bold text-center">
+                CHOOSE PRICE PLAN
+              </h3>
+            </div>
+        
         <div className="grid grid-cols-1 gap-6 mt-8 md:grid-cols-3 md:gap-8">
           <div className="flex flex-col p-6 bg-white shadow-lg rounded-lg dark:bg-zinc-850 justify-between border border-gray-300">
+            
             <div>
               <h3 className="text-2xl font-bold text-center">Basic</h3>
               <div className="mt-4 text-center text-zinc-600 dark:text-zinc-400">
                 <span className="text-4xl font-bold">$29</span>/ month
               </div>
               <ul className="mt-4 space-y-2">
+               
                 <li className="flex items-center">
                   <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
-                  720p Video Rendering
+                  Basic Video Templates
                 </li>
                 <li className="flex items-center">
                   <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
-                  2GB Cloud Storage
+                  Basic Video Templates
                 </li>
                 <li className="flex items-center">
                   <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
+                  Basic Video Templates
+                </li>
+                <li className="flex items-center">
+                  <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
+                  Basic Video Templates
+                </li>
+                <li className="flex items-center">
+                  <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
+                  Basic Video Templates
+                </li>
+                <li className="flex items-center">
+                  <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
+                  Basic Video Templates
+                </li>
+                <li className="flex items-center">
+                  <CheckIcon className="text-white text-xs bg-red-500 rounded-full mr-2 p-1" />
+                  Basic Video Templates
+                </li>
+                <li className="flex items-center">
+                  <CheckIcon className="text-white text-xs bg-red-500 rounded-full mr-2 p-1" />
+                  Basic Video Templates
+                </li>
+                <li className="flex items-center">
+                  <CheckIcon className="text-white text-xs bg-red-500 rounded-full mr-2 p-1" />
+                  Basic Video Templates
+                </li>
+                <li className="flex items-center">
+                  <CheckIcon className="text-white text-xs bg-red-500 rounded-full mr-2 p-1" />
                   Basic Video Templates
                 </li>
               </ul>
@@ -40,22 +77,47 @@ export default function Component() {
                 <span className="text-4xl font-bold">$59</span>/ month
               </div>
               <ul className="mt-4 space-y-2">
-                <li className="flex items-center">
-                  <CheckIcon className="text-white text-2xs bg-green-500 rounded-full mr-2 p-1" />
-                  1080p Video Rendering
+              <li className="flex items-center">
+                  <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
+                  Basic Video Templates
                 </li>
                 <li className="flex items-center">
                   <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
-                  10GB Cloud Storage
+                  Basic Video Templates
                 </li>
                 <li className="flex items-center">
                   <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
-                  Premium Video Templates
+                  Basic Video Templates
                 </li>
                 <li className="flex items-center">
                   <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
-                  Collaboration Tools
+                  Basic Video Templates
                 </li>
+                <li className="flex items-center">
+                  <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
+                  Basic Video Templates
+                </li>
+                <li className="flex items-center">
+                  <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
+                  Basic Video Templates
+                </li>
+                <li className="flex items-center">
+                  <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
+                  Basic Video Templates
+                </li>
+                <li className="flex items-center">
+                  <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
+                  Basic Video Templates
+                </li>
+                <li className="flex items-center">
+                  <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
+                  Basic Video Templates
+                </li>
+                <li className="flex items-center">
+                  <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
+                  Basic Video Templates
+                </li>
+                
               </ul>
             </div>
             <div className="mt-6">
@@ -71,17 +133,10 @@ export default function Component() {
                 <span className="text-4xl font-bold">$99</span>/ month
               </div>
               <ul className="mt-4 space-y-2">
+                
                 <li className="flex items-center">
                   <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
-                  4K Video Rendering
-                </li>
-                <li className="flex items-center">
-                  <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
-                  Unlimited Cloud Storage
-                </li>
-                <li className="flex items-center">
-                  <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
-                  Custom Video Templates
+                  Advanced Collaboration Tools
                 </li>
                 <li className="flex items-center">
                   <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
@@ -89,8 +144,37 @@ export default function Component() {
                 </li>
                 <li className="flex items-center">
                   <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
-                  Dedicated Support
+                  Advanced Collaboration Tools
                 </li>
+                <li className="flex items-center">
+                  <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
+                  Advanced Collaboration Tools
+                </li>
+                <li className="flex items-center">
+                  <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
+                  Advanced Collaboration Tools
+                </li>
+                <li className="flex items-center">
+                  <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
+                  Advanced Collaboration Tools
+                </li>
+                <li className="flex items-center">
+                  <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
+                  Advanced Collaboration Tools
+                </li>
+                <li className="flex items-center">
+                  <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
+                  Advanced Collaboration Tools
+                </li>
+                <li className="flex items-center">
+                  <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
+                  Advanced Collaboration Tools
+                </li>
+                <li className="flex items-center">
+                  <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
+                  Advanced Collaboration Tools
+                </li>
+                
               </ul>
             </div>
             <div className="mt-6">
