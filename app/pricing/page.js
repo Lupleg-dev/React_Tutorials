35c2e21d92ef6b975c2d6e47a36e81566b2b62f2
marksikaundi <emmanuelmark117@gmail.com>
2024-01-05 12:48:56 +0200

    added some black border in price plans

--- app/pricing/page.js
+++ app/pricing/page.js
@@ -12,7 +12,7 @@ export default function PricePlan() {
             </div>
         
         <div className="grid grid-cols-1 gap-6 mt-8 md:grid-cols-3 md:gap-8">
-          <div className="flex flex-col p-6 bg-white shadow-lg rounded-lg dark:bg-zinc-850 justify-between border border-gray-300">
+          <div className="flex flex-col p-6 bg-white shadow-lg rounded-lg dark:bg-zinc-850 justify-between border border-black">
             
             <div>
               <h3 className="text-2xl font-bold text-center">Basic</h3>
@@ -126,7 +126,7 @@ export default function PricePlan() {
               </Button>
             </div>
           </div>
-          <div className="flex flex-col p-6 bg-white shadow-lg rounded-lg dark:bg-zinc-850 justify-between border border-gray-300">
+          <div className="flex flex-col p-6 bg-white shadow-lg rounded-lg dark:bg-zinc-850 justify-between border border-black">
             <div>
               <h3 className="text-2xl font-bold text-center">Enterprise</h3>
               <div className="mt-4 text-center text-zinc-600 dark:text-zinc-400">
