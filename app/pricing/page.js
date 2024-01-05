87defb1581780f8932103c608ab383393355ddcc
marksikaundi <emmanuelmark117@gmail.com>
2024-01-05 12:52:23 +0200

    rn zinc and replaced with black

--- app/pricing/page.js
+++ app/pricing/page.js
@@ -16,7 +16,7 @@ export default function PricePlan() {
             
             <div>
               <h3 className="text-2xl font-bold text-center">Basic</h3>
-              <div className="mt-4 text-center text-zinc-600 dark:text-zinc-400">
+              <div className="mt-4 text-center text-black dark:text-zinc-400">
                 <span className="text-4xl font-bold">$29</span>/ month
               </div>
               <ul className="mt-4 space-y-2">
@@ -73,7 +73,7 @@ export default function PricePlan() {
             </div>
             <div>
               <h3 className="text-2xl font-bold text-center">Pro</h3>
-              <div className="mt-4 text-center text-zinc-600 dark:text-zinc-400">
+              <div className="mt-4 text-center text-black dark:text-zinc-400">
                 <span className="text-4xl font-bold">$59</span>/ month
               </div>
               <ul className="mt-4 space-y-2">
@@ -129,7 +129,7 @@ export default function PricePlan() {
           <div className="flex flex-col p-6 bg-white shadow-lg rounded-lg dark:bg-zinc-850 justify-between border border-black">
             <div>
               <h3 className="text-2xl font-bold text-center">Enterprise</h3>
-              <div className="mt-4 text-center text-zinc-600 dark:text-zinc-400">
+              <div className="mt-4 text-center text-black dark:text-zinc-400">
                 <span className="text-4xl font-bold">$99</span>/ month
               </div>
               <ul className="mt-4 space-y-2">
